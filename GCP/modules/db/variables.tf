ab5e5c0ad3707eff6d021b54971b73e88f65b390
RadhaKrishna-Divakaruni <97581995+RadhaKrishna-Divakaruni@users.noreply.github.com>
2022-03-01 19:13:36 +0530

    Create variables.tf

---
+++ GCP/modules/db/variables.tf
@@ -0,0 +1,34 @@
+variable "mysqldbname" {
+  default = "master instance"
+}
+
+variable "mysqldbversion"{
+  default = "MYSQL_8_0"
+}
+
+variable "mysqlregion" {
+  default = "us-central1"
+}
+
+
+variable "postgredbname" {
+  default = "master1-instance"
+}
+
+
+variable "postgredbversion" {
+  default = "POSTGRES_11"
+}
+
+variable "postgreregion" {
+  default = "us-central1"
+}
+variable "sqlserverdbname" {
+  default = "master-instance"
+}
+variable "sqlserverdbversion" {
+  default = "SQLSERVER_2017_STANDARD"
+}
+variable "sqlserverregion" {
+  default = "us-central1"
+}
