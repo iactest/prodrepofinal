6135401330791bfeb147f8e9eb5266fb854957b1
iactest <103730049+iactest@users.noreply.github.com>
2022-05-02 16:18:54 +0530

    Update main.tf

--- AWS/modules/storage/main.tf
+++ AWS/modules/storage/main.tf
@@ -8,6 +8,11 @@ terraform {
 }
 resource "aws_s3_bucket" "log_bucket"  {
    bucket = "examplebuckettftest"
+   acl    = "private"
+   versioning {
+    enabled = true
+    mfa_delete = true
+  }
 }
 resource "aws_s3_bucket" "logging" {
   bucket = "examplebuckettftest"
